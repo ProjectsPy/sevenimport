1353d439125ad85a7912aa5ad3cacc9e511cdca4
Mainumby <dari_din@hotmail.com>
2015-07-02 16:20:38 -0400

    seven import changes

--- sevenimport/Controllers/marcasImgController.cs
+++ sevenimport/Controllers/marcasImgController.cs
@@ -23,23 +23,24 @@ namespace sevenimport.Controllers
         public ActionResult ListarImagenxProducto(int IdProducto)
         {
 
-            var modelo = from p in db.ProductoImagen
-                         join c in db.Producto
-                             ////on p.IdProducto equals c.IdProducto
-                          on p.IdProducto equals c.IdProducto
-                         where p.IdProducto == IdProducto
+            //var modelo = from p in db.ProductoImagen
+            //             join c in db.Producto
+            //                 ////on p.IdProducto equals c.IdProducto
+            //              on p.IdProducto equals c.IdProducto
+            //             where p.IdProducto == 
 
-                         select new ItemModel
-                         {
-                             ////IdProducto = p.IdProducto,
-                             Descripcion = c.Descripcion,
-                             IdImagen = p.IdImagen,
-                             Imagen = p.Imagen,
-                             Nombre = p.Nombre,
 
-                         };
+            //             select new ItemModel
+            //             {
+            //                 ////IdProducto = p.IdProducto,
+            //                 Descripcion = c.Descripcion,
+            //                 IdImagen = p.IdImagen,
+            //                 Imagen = p.Imagen,
+            //                 Nombre = p.Nombre,
 
-            return View(modelo.ToList());
+            //             };
+
+            //return View(modelo.ToList());
 
 
         }
